a090ee4c86a41b31205868b974f073dd849e4513
Dac099 <davidcejazapata@gmail.com>
2022-09-13 22:41:26 -0500

    Numero de tarjeta funcional

--- interactive-card-details-form-main/modules/fill_cards.js
+++ interactive-card-details-form-main/modules/fill_cards.js
@@ -3,3 +3,17 @@ import { cards } from "./elements.js";
 export function fillSectionCard(element, content){
   element.innerText = content;
 }
+
+export function formatString(arr, str){
+  for(let i = 1; i <= arr.length; i++){
+    if(i%4 === 0){
+      str += arr[i-1] + ' ';
+    }
+  
+    if(i%4 !== 0){
+      str += arr[i-1];
+    }
+  }
+
+  return str.slice(9);
+}
--- interactive-card-details-form-main/modules/read_form.js
+++ interactive-card-details-form-main/modules/read_form.js
@@ -1,5 +1,5 @@
 import { form } from './elements.js';
-import { fillSectionCard } from './fill_cards.js';
+import { fillSectionCard, formatString } from './fill_cards.js';
 import { cards } from './elements.js';
 
 const { name, number, month, year, code } = form;
@@ -7,12 +7,21 @@ const { numbers, date, user_name } = cards.card_front;
 const { cvc } = cards.card_back;
 
 function readingForm(){
+  let card_numbers = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+  let str_numbers;
+
   name.addEventListener('input', (e) => {
     fillSectionCard(user_name, name.value);
   });
 
   number.addEventListener('input', (e) => {
-    fillSectionCard(numbers, number.value);
+    const position = number.value.length - 1;
+
+    card_numbers.splice(position, 1, number.value[position]);
+    
+    console.log(formatString(card_numbers.join(''), str_numbers));
+
+    fillSectionCard(numbers, formatString(card_numbers.join(''), str_numbers));
   });
 
   month.addEventListener('input', (e) => {
